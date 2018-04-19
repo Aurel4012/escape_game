07d2f494a6649ae788dfdec42f5a0724652c910c
aformac_red <aformac_red@192.168.1.183>
2018-04-20 00:24:33 +0200

    test objet axe
    test objet axe

--- functions.php
+++ functions.php
@@ -1,3 +1,30 @@
 <?php 
-    //Les fonctions php pour intérragir avec la base
+    
+
+    function insertItem()
+    {
+        echo "test";
+        
+        $servername = "localhost";
+        $username = "root";
+        $password = "";
+        $dbname = "hackathon";
+        
+        try {
+            $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+
+            $sql = "INSERT INTO Inventaire (id_objet, id_partie) VALUES ('2', '1')";
+ 
+            $conn->exec($sql);
+            echo "New record created successfully";
+        }
+        catch(PDOException $e)
+        {
+            echo $sql . "<br>" . $e->getMessage();
+        }
+        
+        
+        
+    }
+    
 ?>
--- script.js
+++ script.js
@@ -1,1 +1,25 @@
-//Les fonctions permettant les interractions (AJAX partout)
+function clickItem(item)
+{
+	switch (item)
+	{
+		case 1:
+			console.log("Vous avez la hache");
+			insertItem(1);
+			break;
+		default:
+			console.log("noob");
+	
+	}
+}
+
+function insertItem(1)
+{
+	$.ajax({url: "functions.php",
+			type:"post",
+			data:"insertItem"
+			}
+		succes: function(output){
+			console.log("la hache est enregistrée dans la base!");
+		}
+	);
+}
