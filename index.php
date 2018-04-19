1475be96b4a22d0bc9c0cd6d533422d948a17bd6
ezaltar <ezaltar@ezaltar>
2018-04-20 00:14:47 +0200

    Ajout de la vue du menu principal

--- index.php
+++ index.php
@@ -1,3 +1,36 @@
-<?php 
-    //Page d'accueil avec nouvelle partie ou continuer
-?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Escape game de la MORT</title>
+        <link rel="shortcut icon" href="favicon.ico" type="image/vnd.microsoft.icon">
+        <link rel="stylesheet" href="style.css" type="text/css">
+        <script src="script.js"></script>
+
+        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js" integrity="sha384-cs/chFZiN24E4KMATLdqdvsezGxaGsi4hLGOzlXwp5UZB1LY//20VyM2taTB4QvJ" crossorigin="anonymous"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js" integrity="sha384-uefMccjFJAIv6A+rW+L4AHf99KvxDjWSu1z9VI8SKNVmz4sk7buKt/6v9KI65qnm" crossorigin="anonymous"></script>
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css" integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4" crossorigin="anonymous">
+
+    </head>
+    <body>
+
+        <h1 class="text-center menu_space_title">$TITRE</h1>
+
+        <div class="row justify-content-around menu_space_button">
+            <button class="btn"> Nouvelle Partie </button>
+        </div>
+
+        <div class="row justify-content-around menu_space_button">
+            <button class="btn"> Continuer </button>
+        </div>
+
+
+        <div class="row justify-content-around menu_space_dvt">
+            <h6 class="">Développé par : $Nom</h6>
+        </div>
+
+
+
+    </body>
+</html>
