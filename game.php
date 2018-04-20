c7318808a1d1c6dc4cac02f00df2e60637b289f4
ezaltar <ezaltar@ezaltar>
2018-04-20 15:47:48 +0200

    Merge origin/master

--- game.php
+++ game.php
@@ -1,11 +1,11 @@
 <!DOCTYPE html>
 <html>
-	<head>
+    <head>
         <meta charset="UTF-8">
         <title>Escape game de la MORT</title>
         <link rel="shortcut icon" href="favicon.ico" type="image/vnd.microsoft.icon">
         <link rel="stylesheet" href="style.css" type="text/css">
-        
+
 
         <script src="https://code.jquery.com/jquery-3.3.1.min.js" ></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js" integrity="sha384-cs/chFZiN24E4KMATLdqdvsezGxaGsi4hLGOzlXwp5UZB1LY//20VyM2taTB4QvJ" crossorigin="anonymous"></script>
@@ -14,79 +14,79 @@
         <script src="script.js"></script>
 
     </head>
-    
-    
-	<body>
+
+
+    <body>
 <!--        <p><?php echo "Value is: " . $_COOKIE[id_partie]; ?></p>
-        <img alt="imageTestObjet" src="images/axe.png" onclick="clickItem(1)">
-        <img alt="imageTestObjet" src="images/axe.png" onclick="clickItem(2)">-->
+    <img alt="imageTestObjet" src="images/axe.png" onclick="clickItem(1)">
+    <img alt="imageTestObjet" src="images/axe.png" onclick="clickItem(2)">-->
 
 
-		<div class="">
+        <div class="">
             <div class="row">
                 <div class="col-12">
-                <div id="menu" class="">
+                    <div id="menu" class="">
                         <div id="onglets" class="size_navigation dessus_menu row justify-content-around" style="width: 50%; margin-left: 25%;"> 
-                            <button type="submit" class="col-2" onclick="show_menu('salon')" style="padding: 0%;"> <p class="dessus_nom_pièce">Salon</p> <img src="images/item_salon/salon_full.jpg" style="width: 100%;"></button>
-                            <button type="submit" class="col-2" onclick="show_menu('cuisine')" style="padding: 0%;" > <p class="dessus_nom_pièce">Cuisine</p> <img src="images/item_cuisine/cuisine_full.jpg" style="width: 100%;"> </button>
-                            <button type="submit" class="col-2" onclick="show_menu('bureau')"  style="padding: 0%;"> <p class="dessus_nom_pièce">Bureau</p> <img src="images/item_bureau/bureau_full.jpg" style="width: 100%;"> </button>
-                            <button type="submit" class="col-2" onclick="show_menu('garage')" style="padding: 0%;" > <p class="dessus_nom_pièce">Garage</p> <img src="images/item_garage/garage_full.jpg" style="width: 100%;"> </button>
+                            <button type="submit" class="col-2" onclick="show_menu('salon')" style="padding: 0%;"> <p class="dessus_nom_piï¿½ce">Salon</p> <img src="images/item_salon/salon_full.jpg" style="width: 100%;"></button>
+                            <button type="submit" class="col-2" onclick="show_menu('cuisine')" style="padding: 0%;" > <p class="dessus_nom_piï¿½ce">Cuisine</p> <img src="images/item_cuisine/cuisine_full.jpg" style="width: 100%;"> </button>
+                            <button type="submit" class="col-2" onclick="show_menu('bureau')"  style="padding: 0%;"> <p class="dessus_nom_piï¿½ce">Bureau</p> <img src="images/item_bureau/bureau_full.jpg" style="width: 100%;"> </button>
+                            <button type="submit" class="col-2" onclick="show_menu('garage')" style="padding: 0%;" > <p class="dessus_nom_piï¿½ce">Garage</p> <img src="images/item_garage/garage_full.jpg" style="width: 100%;"> </button>
                             <button type="submit" class="col-2" onclick="show_menu('hall')" style="padding: 0%;" > <p class="dessus_hall">Hall</p> <img src="images/item_hall/space-3197639.jpg" style="width: 100%;">  </button>            
                         </div>
-                        
-                      <div id="salon" class=""><img src="images/item_salon/salon_wall.jpg" style="width: 100%" >
-                            <img class="bibiotheque" src="images/item_salon/library-book-cart-vector-graphic.png" onclick="clickItem(2)"/>
+
+                        <div id="salon" class=""><img src="images/item_salon/salon_wall.jpg" style="width: 100%" >
+                            <img class="bibiotheque" src="images/item_salon/library-book-cart-vector-graphic.png" onclick="clickItem(3)"/>
                             <img class="elevator" src="images/item_salon/elevator.png" onclick="clickItem(1)"/>
-                            <img class="baffle"src="images/item_salon/speaker-35556_1280.png" onclick="clickItem(6)"/>
+                            <img class="baffle"src="images/item_salon/speaker-35556_1280.png" onclick="clickItem(2)"/>
                             <img class="baffle2"src="images/item_salon/speaker-35556_1280.png" onclick="clickItem(6)"/>
-                            <img class="tele"src="images/item_salon/tv_star.png" onclick="clickItem(7)"/>
+                            <img class="tele"src="images/item_salon/tv_star.png" onclick="clickItem(4)"/>
                         </div>
-                        
+
                         <div id="cuisine" class="hidden"> <img src="images/item_cuisine/cuisine_full.jpg">
-                         
-   <!--                        <img class="tablee" src="images/item_cuisine/cuisine_full.jpg" onclick="clickItem(8)"/> 
-                          <img class="crevette" src="images/item_cuisine/cartoon-2027717_960_720.png"/> 
-                           
-                            <img class="evier" src="images/item_cuisine/Kitchen_Sink_sprite_002.png" onclick="clickItem(2)"/>
-                            <img class="casserol" src="images\item_cuisine\casserol.png" onclick="clickItem(1)"/>
-                            <img class="kiwi" src="images/item_cuisine/kiwi-161728_960_720.png" onclick="clickItem(3)"/>
-                            <img class="caftiere" src="images/item_cuisine/ec2e8e45.png" onclick="clickItem(4)"/>
-                            <img class="four" src="images\item_cuisine\four_lapin.jpg" onclick="clickItem(5)"/>
-                            <img class="allumette" src="images/item_cuisine/match-159212_960_720.png" onclick="clickItem(6)"/>
-                            <img class="note" src="images/item_cuisine/memory-1293086_1280.png" onclick="clickItem(7)"/>
-                              
-                            <img class="fromage" src="images/item_cuisine/swiss-cheese-575540_960_720.png" onclick="clickItem(8)"/>  -->
-                            
-                        
-                        
+
+<!--                        <img class="tablee" src="images/item_cuisine/cuisine_full.jpg" onclick="clickItem(8)"/> 
+ <img class="crevette" src="images/item_cuisine/cartoon-2027717_960_720.png"/> 
+  
+   <img class="evier" src="images/item_cuisine/Kitchen_Sink_sprite_002.png" onclick="clickItem(2)"/>
+   <img class="casserol" src="images\item_cuisine\casserol.png" onclick="clickItem(1)"/>
+   <img class="kiwi" src="images/item_cuisine/kiwi-161728_960_720.png" onclick="clickItem(3)"/>
+   <img class="caftiere" src="images/item_cuisine/ec2e8e45.png" onclick="clickItem(4)"/>
+   <img class="four" src="images\item_cuisine\four_lapin.jpg" onclick="clickItem(5)"/>
+   <img class="allumette" src="images/item_cuisine/match-159212_960_720.png" onclick="clickItem(6)"/>
+   <img class="note" src="images/item_cuisine/memory-1293086_1280.png" onclick="clickItem(7)"/>
+     
+   <img class="fromage" src="images/item_cuisine/swiss-cheese-575540_960_720.png" onclick="clickItem(8)"/>  -->
+
+
+
                         </div>
-                        
+
                         <div id="bureau" class="hidden">                     
                             <img src="images/item_bureau/bureau_wall.jpg"/>
-                            <img class="bureau" src="images/item_bureau/desk.png" onclick="clickItem(2)"/>
-                            <img class="elevator2" src="images/item_bureau/elevator.png" onclick="clickItem(3)"/>
-                                                        <img class="coffre" src="images/item_bureau/cofre.png" onclick="clickItem(1)"/>
-                                                                                    <img class="joconde" src="images/item_bureau/joconde.png" onclick="clickItem(4)"/>
-                                                        
-                            
-                            <img class="cle"src="images/item_bureau/key.png" onclick="clickItem(5)"/>
-                            <img class="telephone" src="images/item_bureau/phone.png" onclick="clickItem(6)"/>
-                            <img class="torche" src="images/item_bureau/torche.png" onclick="clickItem(7)"/>
-                            <img class="tortue" src="images/item_bureau/turtle.png" onclick="clickItem(8)"/>                                                    
+                            <img class="bureau" src="images/item_bureau/desk.png" onclick="clickItem(31)"/>
+                            <img class="elevator2" src="images/item_bureau/elevator.png" onclick="clickItem(32)"/>
+                            <img class="coffre" src="images/item_bureau/cofre.png" onclick="clickItem(31)"/>
+                            <img class="joconde" src="images/item_bureau/joconde.png" onclick="clickItem(33)"/>
+
+
+                            <img class="cle"src="images/item_bureau/key.png" onclick="clickItem(34)"/>
+                            <img class="telephone" src="images/item_bureau/phone.png" onclick="clickItem(35)"/>
+                            <img class="torche" src="images/item_bureau/torche.png" onclick="clickItem(36)"/>
+                            <img class="tortue" src="images/item_bureau/turtle.png" onclick="clickItem(37)"/>                                                    
                         </div>
-                        
+
                         <div id="garage" class="hidden"> <img src="images/item_garage/garage_full.jpg">
                         </div>
-                        
+
                         <div id="hall" class="hidden"> <img src="images/item_hall/space-3197639.jpg">
-        
+                          
                         </div>
-                    
+
 
                     </div>
                 </div>
-        	</div>
-		</div>
+            </div>
+        </div>
     </body>
 
 </html>
--- script.js
+++ script.js
@@ -1,19 +1,19 @@
 function newGame()
 {
-    	$.ajax({url: "functions.php",
-			type:"post",
-			data:{fnct: 'newGame'},
-			success: function(output){
-				console.log(output);
-                            output = JSON.parse(output);
-                            console.log("nouvelle partie créée, id: " + output);
-                            
-                            document.cookie = "id_partie=" + output; 
-                             
-                            location.href = "game.php";
-			}
-                    });
-                    
+    $.ajax({url: "functions.php",
+        type: "post",
+        data: {fnct: 'newGame'},
+        success: function (output) {
+            console.log(output);
+            output = JSON.parse(output);
+            console.log("nouvelle partie créée, id: " + output);
+
+            document.cookie = "id_partie=" + output;
+
+            location.href = "game.php";
+        }
+    });
+
 
 }
 
@@ -45,47 +45,45 @@ function clickItem(item)
             insertItem(9);
             break;
         case 31:
-            if(checkItem(31) == "cle et code")
+            if (checkItem(31) == "cle et code")
             {
                 showMessage("A l'aide de la clé et du code vous avez déverouillé le coffre-fort!");
-            }
-            else if(checkItem(31) == "cle")
+            } else if (checkItem(31) == "cle")
             {
                 showMessage("Vous avez déverouillé la serrure avec la clé, plus qu'a trouver le code!");
-            }
-            else if(checkItem(31) == "code")
+            } else if (checkItem(31) == "code")
             {
                 showMessage("Vous avez entrer le digicode du code, plus qu'a trouver la clé!");
-            }
-            else
+            } else
             {
                 showMessage("Le coffre est protégé par une serrure et un digicode...");
-            };
+            }
+            ;
             break;
         case 32:
             break;
         case 33:
-            if(checkItem(33) == "torche")
+            if (checkItem(33) == "torche")
             {
                 showMessage("Il fait trop sombre dans la pièce observer en détails ce tableau...");
-            }
-            else
+            } else
             {
                 showMessage("Grâce à la lampe torche vous remarquer que le tableau dissimule une coffre-fort!");
-            };
+            }
+            ;
             break;
         case 34:
             insertItem(34);
             break;
         case 35:
-            if(checkItem(35) == "phone")
+            if (checkItem(35) == "phone")
             {
                 showMessage("Après avoir composé le numéro de la tortue vous entendez une voix vous dire un code: 874652!");
-            }
-            else
+            } else
             {
                 showMessage("Vous ne savez pas quel numéro composer avec ce téléphone...");
-            };
+            }
+            ;
             break;
         case 36:
             insertItem(36);
@@ -118,13 +116,22 @@ function checkItem(item)
         type: "post",
         data: {fnct: 'checkItem', item: item},
         success: function (output) {
-            if(JSON.parse(output))
+            if (JSON.parse(output))
             {
                 return output;
             }
         }
     });
 }
+
+
+
+function showMessage(message)
+{
+    alert(message);
+
+}
+
 
 function show_menu(id_menu) {
     console.log("text");
@@ -161,46 +168,3 @@ function show_menu(id_menu) {
     }
 
 }
-
-function showMessage(message)
-{
-    alert(message);
-                    
-}
-
-	
-	function show_menu(id_menu) {
-		console.log("text");
-	    if (id_menu == "salon") {
-	        $("#salon").removeClass("hidden");
-	        $("#garage").addClass("hidden");
-	        $("#cuisine").addClass("hidden");
-	        $("#bureau").addClass("hidden");
-	        $("#hall").addClass("hidden");
-	    } else if (id_menu == "cuisine") {
-	        $("#cuisine").removeClass("hidden");
-	        $("#garage").addClass("hidden");
-	        $("#salon").addClass("hidden");
-	        $("#bureau").addClass("hidden");
-	        $("#hall").addClass("hidden");
-	    } else if (id_menu == "garage") {
-	        $("#garage").removeClass("hidden");
-	        $("#salon").addClass("hidden");
-	        $("#cuisine").addClass("hidden");
-	        $("#bureau").addClass("hidden");
-	        $("#hall").addClass("hidden");
-	    } else if (id_menu == "bureau") {
-	        $("#bureau").removeClass("hidden");
-	        $("#garage").addClass("hidden");
-	        $("#salon").addClass("hidden");
-	        $("#cuisine").addClass("hidden");
-	        $("#hall").addClass("hidden");
-	    }else if (id_menu == "hall") {
-	        $("#hall").removeClass("hidden");
-	        $("#garage").addClass("hidden");
-	        $("#salon").addClass("hidden");
-	        $("#cuisine").addClass("hidden");
-	        $("#bureau").addClass("hidden");
-	    }
-	        
-	}
