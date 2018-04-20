4fa75e265796814b109ad7b837209d1897e73f07
aformac_red <aformac_red@192.168.1.183>
2018-04-20 04:21:20 +0200

    onglets pièces
    Onglet pièce marche pas

--- script.js
+++ script.js
@@ -22,3 +22,42 @@ function insertItem(item)
 			}
 	);
 }
+}
+	console.log("coucou");
+	alert("ceihfozef");
+	
+	function show_menu(id_menu) {
+		console.log("text");
+	    if (id_menu == "salon") {
+	        $("#salon").removeClass("hidden");
+	        $("#garage").addClass("hidden");
+	        $("#cuisine").addClass("hidden");
+	        $("#bureau").addClass("hidden");
+	        $("#hall").addClass("hidden");
+	    } else if (id_menu == "cuisine") {
+	        $("#cuisine").removeClass("hidden");
+	        $("#garage").addClass("hidden");
+	        $("#salon").addClass("hidden");
+	        $("#bureau").addClass("hidden");
+	        $("#hall").addClass("hidden");
+	    } else if (id_menu == "garage") {
+	        $("#garage").removeClass("hidden");
+	        $("#salon").addClass("hidden");
+	        $("#cuisine").addClass("hidden");
+	        $("#bureau").addClass("hidden");
+	        $("#hall").addClass("hidden");
+	    } else if (id_menu == "bureau") {
+	        $("#bureau").removeClass("hidden");
+	        $("#garage").addClass("hidden");
+	        $("#salon").addClass("hidden");
+	        $("#cuisine").addClass("hidden");
+	        $("#hall").addClass("hidden");
+	    }else if (id_menu == "hall") {
+	        $("#hall").removeClass("hidden");
+	        $("#garage").addClass("hidden");
+	        $("#salon").addClass("hidden");
+	        $("#cuisine").addClass("hidden");
+	        $("#bureau").addClass("hidden");
+	    }
+	        
+	}
